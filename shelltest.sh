74883cc2599e175925edc36c2073ef6f1af0f4af
Dominion-Engr <dominionsamuel6@gmail.com>
2026-04-22 16:08:46 +0100

    shelltest.sh file
    A script file that echos "Hello first_name & last_name"

---
+++ shelltest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+good morning to you all
+
+echo "Hello world"
+
+echo "i love bioinformatics"
+
+FIRST_NAME="DOMINION"
+MIDDLE_NAME="Oore-ofe"
+
+echo "Hello world $FIRST_NAME $MIDDLE_NAME"
+
